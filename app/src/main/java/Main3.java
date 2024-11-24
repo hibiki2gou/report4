83a5616aff0f5bba1979fe48ca7ae96f344a9c70
Hibiki TOYAMA <e245742@ie.u-ryukyu.ac.jp>
2024-11-24 18:44:47 +0900

    report4.option

---
+++ app/src/main/java/Main3.java
@@ -0,0 +1,7 @@
+import jp.ac.uryukyu.ie.e245742.*;
+public class Main3 {
+    public static void main(String[] args) {
+        Hero hero = new Hero ("勇者", 10, 5 );
+        System.out.println(hero);
+    }
+}
